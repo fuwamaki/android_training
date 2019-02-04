c94d221460fb7811785de0108a820b117e20a2f1
fuwamaki <variety.fuwa.3@gmail.com>
2019-02-05 00:33:47 +0900

    add list view on main activity

--- app/src/main/java/com/example/yusakumaki/android_training/MainActivity.kt
+++ app/src/main/java/com/example/yusakumaki/android_training/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.yusakumaki.android_training
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.ArrayAdapter
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val items = Array(20, { i -> "Title-$i" })
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items)
+        mainListView.adapter = adapter
     }
 }
