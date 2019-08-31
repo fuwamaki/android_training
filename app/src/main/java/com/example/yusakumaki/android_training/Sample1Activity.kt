399f800fb1e11ea11287f2d9550db7cdeb232b2f
fuwamaki <yusaku.maki@street-academy.com>
2019-08-31 12:50:54 +0900

    add SampleActivity

---
+++ app/src/main/java/com/example/yusakumaki/android_training/Sample1Activity.kt
@@ -0,0 +1,11 @@
+package com.example.yusakumaki.android_training
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class Sample1Activity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sample1)
+    }
+}
