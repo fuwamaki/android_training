7b23de248ed79de70045968c7d2a95d93c29aef9
fuwamaki <yusaku.maki@street-academy.com>
2019-08-31 12:52:28 +0900

    realize segue from MainActivity to Sample1Activity

--- app/src/main/java/com/example/yusakumaki/android_training/MainActivity.kt
+++ app/src/main/java/com/example/yusakumaki/android_training/MainActivity.kt
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 import android.widget.ArrayAdapter
+import android.content.Intent
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,5 +18,10 @@ class MainActivity : AppCompatActivity() {
         val items = Array(20, { i -> "Title-$i" })
         val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items)
         mainListView.adapter = adapter
+        mainListView.setOnItemClickListener { _, view, i, l ->
+            Toast.makeText(this, "Clicked: ${i}", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, Sample1Activity::class.java)
+            startActivity(intent)
+        }
     }
 }
